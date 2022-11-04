351e1e91662fb2733c093c891e17f9ead56bd2b5
Daniifer18 <danportero6334@gmail.com>
2022-11-04 23:35:53 +0100

    Estilos musicales

---
+++ Utilidad/EstiloMusical.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Utilidad;
+
+enum EstiloMusical{
+
+    case CLASICA;
+    case JAZZ;
+    case ROCK;
+    case METAL;
+    case DISCO;
+    case POP;
+    case TRAP;
+    case REGGAETON;
+
+}
+
+?>
