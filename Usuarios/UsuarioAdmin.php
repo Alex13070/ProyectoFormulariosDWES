41c5d7ca15eb05e965aa4b93cbf478f75264c22a
Daniifer18 <danportero6334@gmail.com>
2022-11-08 20:58:09 +0100

    fin

--- Usuarios/UsuarioAdmin.php
+++ Usuarios/UsuarioAdmin.php
@@ -8,14 +8,14 @@ class UsuarioAdmin extends Usuario{
         Funcion para crear un evento 
     */
     function crearEvento(){
-    
+        return "El usuario admin ha creado un evento";
     }
 
     /*
         Funcion para eliminar un evento 
     */
     function eliminarEvento(){
-        
+        return "El usuario admin ha eliminado un evento";
     }
 
 }
