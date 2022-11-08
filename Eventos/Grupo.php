c5eb0ed71cf505ec93f392be17b2b05658e050a5
Daniifer18 <danportero6334@gmail.com>
2022-11-08 22:14:54 +0100

    f

---
+++ Eventos/Grupo.php
@@ -0,0 +1,33 @@
+<?php
+
+use Utilidad\EstiloMusical;
+
+class Grupo {
+
+    private string $nombre;
+    private EstiloMusical $estilo;
+
+    public function __construct(string $nombre,EstiloMusical $estilo){
+        $this->nombre = $nombre;
+        $this->estilo = $estilo;
+    }
+
+    public function getNombre(){
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre){
+        $this->nombre = $nombre;
+    }
+
+
+    public function getEstilo(){
+        return $this->estilo;
+    }
+
+    public function setEstilo($estilo){
+        $this->estilo = $estilo;
+    }
+}
+
+?>
