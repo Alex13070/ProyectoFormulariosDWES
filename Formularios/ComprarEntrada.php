b673d64875ec1b6070aaa440a8cfba4487caf91e
Daniifer18 <danportero6334@gmail.com>
2022-11-11 01:05:02 +0100

    falta poco

--- Formularios/ComprarEntrada.php
+++ Formularios/ComprarEntrada.php
@@ -58,14 +58,28 @@ function formularioDatosEvento() {?>
     </div>
     <div class="mb-3">
         <label class="form-label">Sexo</label>
-        Hombre:  <input type="radio" name="sexo" required>
-        Mujer: <input type="radio" name="sexo" required>
-        Otro: <input type="radio" name="sexo" required>
+        Hombre:  <input type="radio" name="sexo">
+        Mujer: <input type="radio" name="sexo">
+        Otro: <input type="radio" name="sexo">
         <div class="invalid-feedback">
             Debe seleccionar un sexo.
         </div>
     </div>
-<?php }?>
+<?php }
+
+const TARIFA_MAXIMA_CINE = 20;
+const TARIFA_MAXIMA_CONCIERTO = 120;
+const MINIMO_GENERICO = 0;
+
+
+function generaEventos(array $opciones) { ?>
+    <select class="form-select">
+        <?php foreach($opciones as $clave) { ?>
+            <option disabled value="" selected hidden value="<?= $clave ?>"><?= $clave ?></option>
+        <?php } ?>
+    </select>
+<?php } ?>
+
 
 <!DOCTYPE html>
 <!-- saved from url=(0055)http://localhost:8000/ut4/Prueba/FormularioProyecto.php -->
@@ -106,79 +120,13 @@ function formularioDatosEvento() {?>
 
                                 <?= formularioDatosEvento() ?>
 
-                                <div class="mb-3" id="nombre__evento">
-                                    <label class="form-label">Nombre evento</label>
-                                    <input class="form-control" id="evento" type="text" name="nombre"
-                                        placeholder="Nombre del evento">
-                                </div>
-
                                 <div class="mb-3">
-
-                                    <label class="form-label">Fecha del evento</label>
-                                    <input class="form-control" id="fecha" type="date" name="fecha"
-                                        placeholder="dd/mm/yyyy">
-                                    <p class="formulario_error"></p>
+                                    <label class="form-label">Precio entrada</label>
+                                    <input type="number" class="form-control" min="<?= MINIMO_GENERICO ?>" max="<?= TARIFA_MAXIMA_CINE ?>" name="precio">
                                 </div>
 
-                                <div class="mb-3">
+                                <?= generaEventos($_POST["genero[]"]) ?>
 
-                                    <label class="form-label">Lugar evento</label>
-                                    <input class="form-control" id="lugar" type="text" name="lugar"
-                                        placeholder="Lugar del evento">
-                                    <p class="formulario_error"></p>
-                                </div>
-
-                                <div class="mb-3">
-
-                                    <label class="form-label">Tarifa</label>
-                                    <input class="form-control" id="Tarifa" type="number" name="tarifa" placeholder="tarifa" min="0">
-
-                                </div>
-
-                                <div class="mb-3">
-
-                                    <label class="form-label">Aforo máximo</label>
-                                    <input class="form-control" id="aforo" type="number" name="aforo"
-                                        placeholder="Aforo máximo (0-<?= AFORO_MAXIMO_CINE ?>)" min="0"
-                                        max="<?= AFORO_MAXIMO_CINE ?>">
-                                    <p class="formulario_error"></p>    
-                                </div>
-
-                                <div class="mb-3">
-
-                                    <label class="form-label">Nombre de la película</label>
-                                    <input class="form-control" id="nombrepelicula" type="text" name="nombrepelicula" placeholder="Nombre de la película" pattern="<?=REGEX["texto"]?>" required>
-                                    <div class="invalid-feedback">
-                                        El nombre de la película solo puede contener letras mayúsculas, letras minúsculas y números.
-                                    </div>
-                                </div>
-
-                                <div class="mb-3">
-
-                                    <label class="form-label">Duración de la película</label>
-                                    <input class="form-control" id="duracion" type="number" name="duracion" placeholder="Duración de la película" min="0" required>
-                                    <div class="invalid-feedback">
-                                        La duración de la película debe de ser un número positivo.
-                                    </div>
-                                </div>
-
-                                <div class="mb-3">
-                                    <label class="form-label">Géneros de cine</label>
-                                    
-                                    <?php for($i = 0; $i < count($generos)-1; $i++) {  ?>
-                                        
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" name="genero[]" id="<?=$generos[$i]->value?>"
-                                                value="<?=$generos[$i]->value?>">
-                                            <label class="form-check-label" for="<?=$generos[$i]->value?>"> <?=$generos[$i]->value?> </label>
-                                            <?php if ($i == count($generos)-2) { ?>
-                                                <div class="invalid-feedback">
-                                                    Tiene que haber al menos un checkbox seleccionado.
-                                                </div>
-                                            <?php } ?>
-                                        </div>
-                                    <?php } ?>
-                                </div>
                                 <div class="d-grid gap-2 col-6 mx-auto">
                                     <input type="submit" value="<?= CINE ?>" class="btn btn-primary" name="enviar">
                                 </div>
@@ -187,36 +135,17 @@ function formularioDatosEvento() {?>
                         </div>
 
                         <div id="concierto">
-                            <form action="CrearEvento.php" method="post" id="formularioConcierto" class="needs-validation <?= ($noExitoso && isset($_POST["enviar"]) && $_POST["enviar"] === CONCIERTO) ? "was-validated":"" ?>" novalidate>
+                            <form action="ComprarEntrada.php" method="post" id="formularioConcierto" class="needs-validation <?= ($noExitoso && isset($_POST["enviar"]) && $_POST["enviar"] === CONCIERTO) ? "was-validated":"" ?>" novalidate>
 
                                 <?= formularioDatosEvento() ?>
 
                                 <div class="mb-3">
-
-                                    <label class="form-label">Nombre del grupo</label>
-                                    <input class="form-control" id="nombregrupo" type="text" name="nombregrupo" placeholder="Nombre del grupo" pattern="<?=REGEX["texto"]?>" required>
-                                    <div class="invalid-feedback">
-                                        Please choose a username.
-                                    </div>
+                                    <label class="form-label">Precio entrada</label>
+                                    <input type="number" class="form-control" min="<?= MINIMO_GENERICO ?>" max="<?= TARIFA_MAXIMA_CONCIERTO ?>" name="precio">
                                 </div>
 
-                                <div class="mb-3">
-
-                                    <label class="form-label">Género musical</label>
-                                    <select class="form-select" aria-label="Default select example" name="generomusical" id="generomusical" required>
-                                        <option disabled value="" selected hidden>Género musical</option>
-                                        
-                                        <?php for($i = 0; $i < count($estilos)-1; $i++) { ?>
-                                        
-                                            <option value="<?=$estilos[$i]->value?>"><?=$estilos[$i]->value?></option>
-
-                                        <?php } ?>
-
-                                    </select>
-                                    <div class="invalid-feedback">
-                                        Please choose a username.
-                                    </div>
-                                </div>
+                                <?= generaEventos($_POST["genero[]"]) ?>
+                                
                                 <div class="d-grid gap-2 col-6 mx-auto">
                                     <input type="submit" value="<?= CONCIERTO ?>" class="btn btn-primary" name="enviar">
                                 </div>
