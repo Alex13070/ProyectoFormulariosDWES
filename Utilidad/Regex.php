62b2faf875a747af197238f493da3807f42e4298
Alex <98185072+Alex13070@users.noreply.github.com>
2022-11-11 03:20:00 +0100

    Regex mejoradas

--- Utilidad/Regex.php
+++ Utilidad/Regex.php
@@ -1,9 +1,27 @@
 <?php
 
+namespace Utilidad;
+
 enum Regex : string {
 
-    case NOMBRE = '[a-zA-Z1-9À-ÖØ-öø-ÿ]+\.?(( |\-)[a-zA-Z1-9À-ÖØ-öø-ÿ]+\.?)*';
-    case NUMERO = '[0-9]{1,}';
-    case TELEFONO = '#^\(?\d{2}\)?[\s\.-]?\d{4}[\s\.-]?\d{4}$#';
+    /**
+     * Expresion regular que permite letras, mayusculas, minusculas, numeros, acentos y caracteres especiales
+     */
+    case NOMBRE = '[a-zA-Z0-9À-ÖØ-öø-ÿ\s\/._-]{1,}';
+
+    /**
+     * Valida numeros y puedes poner si son negativos   
+     */
+    case NUMERO = '[-]?[0-9]{1,}';
+    
+    /**
+     * Expresion regular que pide 9 digitos
+     */
+    case TELEFONO = '\d{9}';
+
+    /**
+     * Valida correos del formato "xxx@xxx.xxx" pudiendo meter letras, numeros, puntos, guiones y barras bajas
+     * 
+     */
     case CORREO = '[A-z0-9\\._-]+@[A-z0-9][A-z0-9-]*(\\.[A-z0-9_-]+)*\\.([A-z]{2,6})';
 }
