764bcbde611711609f3d31a83eb0b149be011ef2
Daniifer18 <danportero6334@gmail.com>
2022-11-15 00:01:16 +0100

    rr

--- Desde_0/Utilidad/EscribirFichero.php
+++ Desde_0/Utilidad/EscribirFichero.php
@@ -1,39 +1,75 @@
 <?php
 
+namespace Desde_0\Utilidad;
+use Exception;
 
-$array = $_POST['Enviar'];
-$salir = false;
-$cadena = "";
+class EscribirFichero{
 
-for($i = 0;$i < count($array) && !$salir;$i++){
-
-    if(!empty($array[$i])){
-        if($i == $array[count($array) -1]){
-            $cadena += $array[$i] . "\n";
-        }else{
-            $cadena += $array[$i] . ";";
+    private array $array = [];
+    private bool $salir = false;
+    private string $cadena = "";
+    
+    private static EscribirFichero $singletone;
+    
+    
+    private function __construct(HttpMethod $method) {
+        switch ($method) {
+            case HttpMethod::GET:
+                $this->array = $_GET;
+                break;
+            case HttpMethod::POST:
+                $this->array = $_POST;
+                break;
+            default:
+                throw new Exception("Método no soportado.");
         }
-    }else $salir = true;
+    }
+
+    public function relllenarFichero(){
+
+        for($i = 0;$i < count($this->array) && !$this->salir;$i++){
+    
+            if(!empty($array[$i])){
+                if($i == $this->array[count($this->array) -1]){
+                    $this->cadena += $this->array[$i] . "\n";
+                }else{
+                    $this->cadena += $this->array[$i] . ";";
+                }
+            }else $this->salir = true;
+        
+        }
+
+    }
+
+    public function getSingletone($method) : EscribirFichero {
+        return is_null(EscribirFichero::$singletone) ? new EscribirFichero($method) : EscribirFichero::$singletone;
+    }
+
+
+    public function getSalir(){
+        return $this->salir;
+    }
+
+    public function setSalir($salir){
+        $this->salir = $salir;
+
+        return $this;
+    }
+
+    public function getArray(){
+        return $this->array;
+    }
+
+    public function setArray($array){
+        $this->array = $array;
+
+        return $this;
+    }
+
+    public function getCadena(){
+        return $this->cadena;
+    }
 
 }
-
-
-if(!$salir){
-    
-    //Guardar
-    file_put_contents(
-        "Eventos.csv",
-        $cadena,
-        FILE_APPEND
-    );
-
-    //Redireccionar
-    header("Location: listado.php");
-
-    //salir
-    exit();
-    
-}
-
 
 ?>
